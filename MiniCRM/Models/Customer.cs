c8fd8bcff148bd9c57efe8b5accdc9efd5bdebc1
Dominic <dominic.nussgraber@gmail.com>
2025-05-05 12:13:14 +0200

    Create project

---
+++ MiniCRM/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniCRM.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [MaxLength(200)]
+        public string Email { get; set; } = string.Empty;
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation Property
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ MiniCRM/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniCRM.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal TotalPrice { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        // Foreign Key
+        public int CuctomerId { get; set; }
+
+        // Navigation Property
+        public Customer? Customer { get; set; }
+    }
+}
